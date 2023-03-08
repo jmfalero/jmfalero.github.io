4720d87cc7349babf6f64251746c58b4fdeb6c20
jmfalero <falero_juanmanuel@hotmail.com>
2023-03-08 13:27:27 -0300

    segundaentrega

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import NavBar from "./components/NavBar";
 import React from "react";
-import Landing from "./components/Landing";
-import Footer from "./components/Footer.jsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Footer from "./components/Footer";
 import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import NavBar from "./components/NavBar";
+
+import Error404 from "./components/Error404";
 
 function App() {
   return (
-    <div >
-      <NavBar />
-      <ItemListContainer greeting={"Pagina en construccion, sin productos!"} />
-      <Landing />
-      <Footer />
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path={"/"} element={<ItemListContainer />} />
+          <Route path={"/category/:id"} element={<ItemListContainer />} />
+          <Route path={"/item/:id"} element={<ItemDetailContainer />} />
+          <Route path={"*"} element={<Error404 />} />
+        </Routes>
+        
+        <Footer />
+      </BrowserRouter>
     </div>
-       
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,43 +1,45 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import CartWidget from "./CartWidget";
 
 const NavBar = () => {
     return (
-          <div className="container">
-           <div className="row">
-                <div className="col-md-6">
-                <nav className="navbar navbar-expand-lg bg-light">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/"><img src={"images/loguito.png"} alt= {"PerformanceFirst"} width={80}></img></a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                            <a className="nav-link" href="/Headers">Headers</a>
-                            </li>
-                            <li className="nav-item">
-                            <a className="nav-link" href="/CoilOvers">CoilOvers</a>
-                            </li>
-                            <li className="nav-item">
-                            <a className="nav-link" href="/Exhasuts">Exhasuts</a>
-                            </li>
-                            <li className="nav-item">
-                            <a className="nav-link" href="/Camshafts">Camshafts</a>
-                            </li>
-                        </ul>
-                       </div>
-                    </div>
-                    </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                  <Link className="navbar-brand" to={"/"}><img src={"images/loguito.png"} alt={"imangenlogo"} width="80"></img></Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span className="navbar-toggler-icon"></span>
+                    </button>
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li className="nav-item">
+                        <NavLink className="nav-link active" aria-current="page" to={"/category/Header"}>Header</NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink className="nav-link active" aria-current="page" to={"/category/Exhaust"}>Exhaust</NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink className="nav-link active" aria-current="page" to={"/category/Camshafts"}>Camshafts</NavLink>
+                      </li>  
+                      <li className="nav-item">
+                        <NavLink className="nav-link active" aria-current="page" to={"/category/Coilovers"}>Coilovers</NavLink>
+                      </li>   
+                    </ul> 
+                  </div>
                 </div>
-                <div className="col-md-6 d-flex justify-content-end align-items-center">
-                    <CartWidget />
-                </div> 
+              </nav>
+          </div>        
+          
+            <div className= "col-md-6 d-flex justify-content-end align-items-center">
+              <CartWidget/>
             </div>
-        </div>
-    )
+          </div>    
+        </div>    
+      )
+  }
 
-}
 
 export default NavBar;
